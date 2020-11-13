1e1a3bf3d82b07e854a6c7d3737158de9c45d96b
Safayet N Ahmed <sahmed62@jhu.edu>
2020-11-13 00:51:11 -0500

    lscache.sh: formatting changes

--- utils/lscache.sh
+++ utils/lscache.sh
@@ -23,20 +23,29 @@ function check_read_file {
 
 ROOTDIR="/sys/devices/system/cpu"
 echo "System {"
-for cpu in $(find "${ROOTDIR}" -mindepth 1 -maxdepth 1 -name "cpu*" -not -name "cpufreq" -not -name "cpuidle" | sort ) ; do
+for cpu in $(find "${ROOTDIR}" \
+                    -mindepth 1 \
+                    -maxdepth 1 \
+                    -name "cpu*" \
+                    -not -name "cpufreq" -not -name "cpuidle" \
+                    | sort ) ; do
     CPUCACHEDIR="${cpu}/cache"
     echo "    $(basename ${cpu}) {"
-    for index in $(find "${CPUCACHEDIR}" -mindepth 1 -maxdepth 1 -name "index*" | sort ) ; do
+    for index in $(find "${CPUCACHEDIR}" \
+                        -mindepth 1 \
+                        -maxdepth 1 \
+                        -name "index*" \
+                        | sort ) ; do
         CPUCACHEINDEXDIR="${index}"
         echo "        $(basename ${index}) {"
-        echo "            level:                   $( check_read_file ${CPUCACHEINDEXDIR}/level)"
-        echo "            type:                    $( check_read_file ${CPUCACHEINDEXDIR}/type)"
-        echo "            id:                      $( check_read_file ${CPUCACHEINDEXDIR}/id)"
-        echo "            shared w/ CPUs:          $( check_read_file ${CPUCACHEINDEXDIR}/shared_cpu_list)"
-        echo "            line size:               $( check_read_file ${CPUCACHEINDEXDIR}/coherency_line_size)"
-        echo "            associativity:           $( check_read_file ${CPUCACHEINDEXDIR}/ways_of_associativity)"
-        echo "            sets:                    $( check_read_file ${CPUCACHEINDEXDIR}/number_of_sets)"
-        echo "            total size:              $( check_read_file ${CPUCACHEINDEXDIR}/size)"
+        echo "            level:            $( check_read_file ${CPUCACHEINDEXDIR}/level)"
+        echo "            type:             $( check_read_file ${CPUCACHEINDEXDIR}/type)"
+        echo "            id:               $( check_read_file ${CPUCACHEINDEXDIR}/id)"
+        echo "            shared w/ CPUs:   $( check_read_file ${CPUCACHEINDEXDIR}/shared_cpu_list)"
+        echo "            line size:        $( check_read_file ${CPUCACHEINDEXDIR}/coherency_line_size)"
+        echo "            associativity:    $( check_read_file ${CPUCACHEINDEXDIR}/ways_of_associativity)"
+        echo "            sets:             $( check_read_file ${CPUCACHEINDEXDIR}/number_of_sets)"
+        echo "            total size:       $( check_read_file ${CPUCACHEINDEXDIR}/size)"
         echo "        }"
     done
     echo "    }"
