6a8a9e3175b232038ab25e8c62fc2af10bc8ca65
Safayet N Ahmed <sahmed62@jhu.edu>
2020-11-18 00:12:12 -0500

    check-system.sh: more features to fix errors
    The system was hanging for unknown reasons. The dmesg output
    showed kernel panics related to RCU stalls and hung task. The
    initial "check" script was modified with additional checks of
    kernel boot arguments and build configs to potentially address
    those problems.

--- utils/check-system.sh
+++ utils/check-system.sh
@@ -13,6 +13,11 @@ if [ -r /proc/config.gz ] ; then
     else
         echo "    WARNING: does not have CPU-isolation support"
     fi
+    if zgrep "CONFIG_RCU_NOCB_CPU=y" /proc/config.gz >& /dev/null ; then
+        echo "    supports rcu_nocb. Good!"
+    else
+        echo "    WARNING: does not have rcu_nocb support"
+    fi
     if zgrep "CONFIG_PREEMPT=y" /proc/config.gz >& /dev/null ; then
         echo "    supports CONFIG_PREEMPT. Good!"
     else
@@ -24,6 +29,11 @@ elif [ -r "/boot/config-$(uname -r)" ] ; then
         echo "    supports CPU isolation. Good!"
     else
         echo "    WARNING: does not have CPU-isolation support"
+    fi
+    if grep "CONFIG_RCU_NOCB_CPU=y" "/boot/config-$(uname -r)" >& /dev/null ; then
+        echo "    supports rcu_nocb. Good!"
+    else
+        echo "    WARNING: does not have rcu_nocb support"
     fi
     if grep "CONFIG_PREEMPT=y" "/boot/config-$(uname -r)" >& /dev/null ; then
         echo "    supports CONFIG_PREEMPT. Good!"
@@ -40,8 +50,21 @@ if [ ! -r "/proc/cmdline" ] ; then
 elif ! grep ' isolcpus=' "/proc/cmdline"  >& /dev/null ; then
     echo "WARNING: kernel booted without \"isolcpus=\" command-line argument"
     echo "          Add \"isolcpus=\" boot arg to GRUB_CMDLINE_LINUX_DEFAULT in \"/etc/default/grub\" and run update-grub"
+elif ! grep ' rcu_nocb=' "/proc/cmdline"  >& /dev/null ; then
+    echo "WARNING: kernel booted without \"rcu_nocb=\" command-line argument"
+    echo "          Add \"rcu_nocb=\" boot arg to GRUB_CMDLINE_LINUX_DEFAULT in \"/etc/default/grub\" and run update-grub"
+elif ! grep ' rcu_nocb_poll' "/proc/cmdline"  >& /dev/null ; then
+    echo "WARNING: kernel booted without \"rcu_nocb_poll\" command-line argument"
+    echo "          Add \"rcu_nocb_poll\" boot arg to GRUB_CMDLINE_LINUX_DEFAULT in \"/etc/default/grub\" and run update-grub"
 else
-    echo "Kernel booted with CPU isolation enabled. Good!"
+    echo "Kernel booted with CPU isolation, rcu_nocb, and rcu_nocb_poll enabled. Good!"
     grep ' isolcpus=' "/proc/cmdline"
 fi
 
+if [ -f /sys/module/rcutree/parameters/kthread_prio ] \
+    && [ $(cat /sys/module/rcutree/parameters/kthread_prio) -lt 10 ]; then
+    echo "WARNING: RCU kthread scheduling priority less than 10. This may cause the system to hang."
+    echo "          Add \"rcutree.kthread_prio=10\" boot arg to GRUB_CMDLINE_LINUX_DEFAULT in \"/etc/default/grub\" and run update-grub"
+else
+    echo "RCU kthread scheduling priority not less than 10. Good!"
+fi
